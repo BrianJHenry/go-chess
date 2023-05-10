05271868d1405f6910f2f1be40bda9d554c975e6
Brian Henry <brianjhenry@gmail.com>
2023-05-10 16:09:45 -0500

    ChessGame implementation

--- pkg/models/chess-game.go
+++ pkg/models/chess-game.go
@@ -1,7 +1,19 @@
 package models
 
 type ChessGame struct {
-	currentState     ChessState
-	currentMoveIndex int
-	moves            []Move
+	CurrentState *ChessState
+	Moves        []Move
+}
+
+func NewChessGame() *ChessGame {
+	return &ChessGame{
+		CurrentState: NewChessState(),
+		Moves:        make([]Move, 0, 64),
+	}
+}
+
+func (game *ChessGame) ExecuteMove(move Move) {
+	game.Moves = append(game.Moves, move)
+	game.CurrentState = game.CurrentState.progressStateForward(move)
+	game.CurrentState.board.printChessBoard()
 }

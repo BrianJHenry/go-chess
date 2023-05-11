1d3f240b7fe04f5c9a9e833c4c423ba00716aada
Brian Henry <brianjhenry@gmail.com>
2023-05-10 20:56:40 -0500

    Created dummy functions

--- pkg/controllers/chess-controller.go
+++ pkg/controllers/chess-controller.go
@@ -1,15 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetGame(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{"msg": "Get Game Endpoint"})
 }
 
 func UpdateState(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{"msg": "Update State Endpoint"})
 }
 
 func PlayMove(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{"msg": "Play Move Endpoint"})
 }

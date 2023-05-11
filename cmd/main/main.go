76ad61629769af87bd314f665cd9b5fc42fc40a2
Brian Henry <brianjhenry@gmail.com>
2023-05-10 20:56:54 -0500

    Added dependencies

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
-
-	"github.com/BrianJHenry/go-chess/pkg/models"
+	"github.com/BrianJHenry/go-chess/pkg/routes"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	start := time.Now()
-	game := models.NewChessGame()
-	for i := 0; i < 40; i++ {
-		possibleMoves := game.CurrentState.EnumerateMoves()
-		game.ExecuteMove(possibleMoves[rand.Intn(len(possibleMoves))])
-	}
-	fmt.Printf("Completed %v half moves in %v\n", 100, time.Since(start))
+	router := gin.Default()
+
+	routes.BuildRoutes(router)
+
+	router.Run(":8080")
 }

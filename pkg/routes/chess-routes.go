844e21c0ec811d1e9a7177df1a8126d2387a8bce
Brian Henry <brianjhenry@gmail.com>
2023-05-10 20:22:57 -0500

    Initial Commit

--- pkg/routes/chess-routes.go
+++ pkg/routes/chess-routes.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"github.com/BrianJHenry/go-chess/pkg/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func BuildRoutes(router *gin.Engine) {
+	router.GET("/play", controllers.GetGame)
+	router.GET("/play/:id/updateState", controllers.UpdateState)
+	router.PUT("/play/:id/playMove", controllers.PlayMove)
+}

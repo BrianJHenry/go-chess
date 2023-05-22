a19a6ee2b02c1fb9f9628d12b1b9f3971a73e9b8
Brian Henry <brianjhenry@gmail.com>
2023-05-22 12:40:43 -0500

    Added end of game check

--- server/pkg/models/chess-game.go
+++ server/pkg/models/chess-game.go
@@ -1,18 +1,48 @@
 package models
 
+type Result string
+
+const (
+	ContinueGame Result = "C"
+	Stalemate    Result = "S"
+	WhiteWins    Result = "W"
+	BlackWins    Result = "B"
+)
+
 type ChessGame struct {
-	CurrentState *ChessState
-	Moves        []Move
+	CurrentState  *ChessState
+	MoveHistory   []Move
+	PossibleMoves []Move
+	Winner        Result
 }
 
 func NewChessGame() ChessGame {
-	return ChessGame{
+	game := ChessGame{
 		CurrentState: NewChessState(),
-		Moves:        make([]Move, 0, 64),
+		MoveHistory:  make([]Move, 0, 64),
+		Winner:       ContinueGame,
 	}
+	game.PossibleMoves = game.CurrentState.EnumerateMoves()
+	return game
 }
 
 func (game *ChessGame) ExecuteMoveOnGame(move Move) {
-	game.Moves = append(game.Moves, move)
+	game.MoveHistory = append(game.MoveHistory, move)
 	game.CurrentState = game.CurrentState.ExecuteMoveOnState(move)
+	game.PossibleMoves = game.CurrentState.EnumerateMoves()
+	if len(game.PossibleMoves) == 0 {
+		if game.CurrentState.Turn == White {
+			if game.CurrentState.Board.IsWhiteInCheck() {
+				game.Winner = BlackWins
+			} else {
+				game.Winner = Stalemate
+			}
+		} else if game.CurrentState.Turn == Black {
+			if game.CurrentState.Board.IsBlackInCheck() {
+				game.Winner = WhiteWins
+			} else {
+				game.Winner = Stalemate
+			}
+		}
+	}
 }

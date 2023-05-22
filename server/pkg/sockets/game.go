11e583e2d7a5f5640124e861b3d94ef9f362e750
Brian Henry <brianjhenry@gmail.com>
2023-05-21 20:43:31 -0500

    Added functionality to convert chess models into what the client expects

--- server/pkg/sockets/game.go
+++ server/pkg/sockets/game.go
@@ -6,6 +6,88 @@ import (
 	"github.com/BrianJHenry/go-chess/server/pkg/models"
 )
 
+// setup move information to be sent across websockets
+var moveTypesArray [8]string = [8]string{"N", "S", "L", "P", "Q", "R", "B", "K"}
+var moveTypesMap map[string]int = map[string]int{
+	"N": 0,
+	"S": 1,
+	"L": 2,
+	"P": 3,
+	"Q": 4,
+	"R": 5,
+	"B": 6,
+	"K": 7,
+}
+
+type moveToSend struct {
+	moveType  string `json:"moveType"`
+	oldSquare int    `json:"oldSquare"`
+	newSquare int    `json:"newSquare"`
+}
+
+func convertToMoveToSend(move models.Move) moveToSend {
+	moveType := moveTypesArray[move.Type]
+	oldSquare := move.OldSquare.Row*8 + move.OldSquare.Col
+	newSquare := move.NewSquare.Row*8 + move.NewSquare.Col
+
+	return moveToSend{
+		moveType:  moveType,
+		oldSquare: oldSquare,
+		newSquare: newSquare,
+	}
+}
+
+func convertToMove(move moveToSend) models.Move {
+	moveType := moveTypesMap[move.moveType]
+	oldSquare := models.Location{
+		Row: move.oldSquare / 8,
+		Col: move.oldSquare % 8,
+	}
+	newSquare := models.Location{
+		Row: move.newSquare / 8,
+		Col: move.newSquare % 8,
+	}
+
+	return models.Move{
+		Type:      models.MoveType(moveType),
+		OldSquare: oldSquare,
+		NewSquare: newSquare,
+	}
+}
+
+// setup states to be sent across websockets
+type stateToSend struct {
+	turn          bool
+	board         []int8
+	previousMoves []moveToSend
+}
+
+func convertToStateToSend(game models.ChessGame, ownColor int) stateToSend {
+	var board = make([]int8, 0, 64)
+	for _, row := range game.CurrentState.Board {
+		sliceRow := row[:]
+		board = append(board, sliceRow...)
+	}
+	var turn bool
+	if ownColor == int(game.CurrentState.Turn) {
+		turn = true
+	} else {
+		turn = false
+	}
+	var previousMoves = make([]moveToSend, 0, len(game.Moves))
+	for _, move := range game.Moves {
+		convertedMove := convertToMoveToSend(move)
+		previousMoves = append(previousMoves, convertedMove)
+	}
+
+	return stateToSend{
+		turn:          turn,
+		board:         board,
+		previousMoves: previousMoves,
+	}
+}
+
+// actual game logic
 type Game struct {
 	// game info
 	NumberOfPlayers int
@@ -45,11 +127,10 @@ func (game *Game) Start() {
 			fmt.Println("Number of players in lobby: ", len(game.Clients))
 			// if we have enough players start the game
 			if currentClientCount == game.NumberOfPlayers {
-				// send over board to all
-				// send whose turn to all
 				for i, c := range game.Clients {
 					fmt.Println(i, c)
-					// TODO: send board state, turn, and previous moves as json to client
+					startingState := convertToStateToSend(chessGame, i)
+					client.Conn.WriteJSON(startingState)
 				}
 			}
 		case client := <-game.Unregister:
